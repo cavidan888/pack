d4969fe0aa1045c0d25e63d1dfc0534187fa989a
cavidan888 <cavidan666@gmail.com>
2018-07-05 12:45:24 +0400

    gt

---
+++ Packman/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Packman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.staryer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maneya2 = new System.Windows.Forms.Button();
+            this.pack = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.maniaTop = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.littleObs = new System.Windows.Forms.Button();
+            this.obstacles3 = new System.Windows.Forms.Button();
+            this.oobstacles2 = new System.Windows.Forms.Button();
+            this.obstacles4 = new System.Windows.Forms.Button();
+            this.obstacles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // staryer
+            // 
+            this.staryer.Enabled = false;
+            this.staryer.Location = new System.Drawing.Point(516, 434);
+            this.staryer.Name = "staryer";
+            this.staryer.Size = new System.Drawing.Size(75, 23);
+            this.staryer.TabIndex = 1;
+            this.staryer.Text = "Start Game";
+            this.staryer.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 444);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "[Your Score]";
+            // 
+            // maneya2
+            // 
+            this.maneya2.Enabled = false;
+            this.maneya2.Location = new System.Drawing.Point(135, 127);
+            this.maneya2.Name = "maneya2";
+            this.maneya2.Size = new System.Drawing.Size(10, 53);
+            this.maneya2.TabIndex = 25;
+            this.maneya2.Text = "button1";
+            this.maneya2.UseVisualStyleBackColor = true;
+            // 
+            // pack
+            // 
+            this.pack.BackColor = System.Drawing.Color.Yellow;
+            this.pack.Enabled = false;
+            this.pack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pack.Location = new System.Drawing.Point(170, 137);
+            this.pack.Name = "pack";
+            this.pack.Size = new System.Drawing.Size(20, 13);
+            this.pack.TabIndex = 24;
+            this.pack.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            this.button14.Enabled = false;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button14.Location = new System.Drawing.Point(320, 279);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(14, 70);
+            this.button14.TabIndex = 22;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(320, 135);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(14, 70);
+            this.button9.TabIndex = 21;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(243, 235);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(128, 16);
+            this.button10.TabIndex = 20;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Enabled = false;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Location = new System.Drawing.Point(398, 217);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(14, 52);
+            this.button12.TabIndex = 19;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Enabled = false;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Location = new System.Drawing.Point(273, 279);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(14, 70);
+            this.button13.TabIndex = 18;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Enabled = false;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Location = new System.Drawing.Point(196, 217);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(14, 52);
+            this.button11.TabIndex = 17;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(273, 135);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(14, 70);
+            this.button8.TabIndex = 23;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(480, 286);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(13, 61);
+            this.button7.TabIndex = 16;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(123, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(13, 61);
+            this.button2.TabIndex = 15;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(480, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(13, 58);
+            this.button4.TabIndex = 14;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // maniaTop
+            // 
+            this.maniaTop.Enabled = false;
+            this.maniaTop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.maniaTop.Location = new System.Drawing.Point(135, 114);
+            this.maniaTop.Name = "maniaTop";
+            this.maniaTop.Size = new System.Drawing.Size(55, 13);
+            this.maniaTop.TabIndex = 13;
+            this.maniaTop.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(427, 114);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(55, 13);
+            this.button5.TabIndex = 12;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(438, 346);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(55, 13);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // littleObs
+            // 
+            this.littleObs.Enabled = false;
+            this.littleObs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.littleObs.Location = new System.Drawing.Point(135, 346);
+            this.littleObs.Name = "littleObs";
+            this.littleObs.Size = new System.Drawing.Size(55, 13);
+            this.littleObs.TabIndex = 10;
+            this.littleObs.UseVisualStyleBackColor = true;
+            // 
+            // obstacles3
+            // 
+            this.obstacles3.Enabled = false;
+            this.obstacles3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.obstacles3.Location = new System.Drawing.Point(542, 70);
+            this.obstacles3.Name = "obstacles3";
+            this.obstacles3.Size = new System.Drawing.Size(14, 352);
+            this.obstacles3.TabIndex = 8;
+            this.obstacles3.UseVisualStyleBackColor = true;
+            // 
+            // oobstacles2
+            // 
+            this.oobstacles2.Enabled = false;
+            this.oobstacles2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.oobstacles2.Location = new System.Drawing.Point(81, 70);
+            this.oobstacles2.Name = "oobstacles2";
+            this.oobstacles2.Size = new System.Drawing.Size(462, 18);
+            this.oobstacles2.TabIndex = 7;
+            this.oobstacles2.UseVisualStyleBackColor = true;
+            // 
+            // obstacles4
+            // 
+            this.obstacles4.Enabled = false;
+            this.obstacles4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.obstacles4.Location = new System.Drawing.Point(81, 404);
+            this.obstacles4.Name = "obstacles4";
+            this.obstacles4.Size = new System.Drawing.Size(462, 18);
+            this.obstacles4.TabIndex = 9;
+            this.obstacles4.UseVisualStyleBackColor = true;
+            // 
+            // obstacles
+            // 
+            this.obstacles.Enabled = false;
+            this.obstacles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.obstacles.Location = new System.Drawing.Point(68, 70);
+            this.obstacles.Name = "obstacles";
+            this.obstacles.Size = new System.Drawing.Size(14, 352);
+            this.obstacles.TabIndex = 6;
+            this.obstacles.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(624, 493);
+            this.Controls.Add(this.maneya2);
+            this.Controls.Add(this.pack);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.maniaTop);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.littleObs);
+            this.Controls.Add(this.obstacles3);
+            this.Controls.Add(this.oobstacles2);
+            this.Controls.Add(this.obstacles4);
+            this.Controls.Add(this.obstacles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.staryer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button staryer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button maneya2;
+        private System.Windows.Forms.Button pack;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button maniaTop;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button littleObs;
+        private System.Windows.Forms.Button obstacles3;
+        private System.Windows.Forms.Button oobstacles2;
+        private System.Windows.Forms.Button obstacles4;
+        private System.Windows.Forms.Button obstacles;
+    }
+}
+
---
+++ Packman/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Packman
+{
+    public partial class Form1 : Form
+    {
+        public int leftPos = 12;
+        public int topPos = 12;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Crash();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                pack.Left += leftPos;
+                
+
+               
+
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                pack.Top += topPos;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                pack.Left -= leftPos;
+
+
+                if (pack.Left <= obstacles.Left+obstacles.Width)
+                {
+
+                    pack.Left = leftPos;
+                }
+
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                pack.Top -= topPos;
+
+                
+            }
+        }
+
+        public void Crash()
+        {
+          
+
+        }
+    }
+}
